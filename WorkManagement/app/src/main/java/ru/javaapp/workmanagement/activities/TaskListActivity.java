dd4f1735ab47953840cb37bcc44c628cb405dd80
ildar2244 <ildar2244@gmail.com>
2015-10-01 12:15:48 +0300

    Select spinner UsersType and start Intent

---
+++ WorkManagement/app/src/main/java/ru/javaapp/workmanagement/activities/TaskListActivity.java
@@ -0,0 +1,15 @@
+package ru.javaapp.workmanagement.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ru.javaapp.workmanagement.R;
+
+public class TaskListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_list);
+    }
+}
