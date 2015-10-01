128ffa36f82a6146b45ee57ea71b43336dc12cc9
Ruslan Kalimullin <kalimruslan@mail.ru>
2015-10-01 14:33:30 +0300

    swвапа

--- WorkManagement/app/src/main/java/ru/javaapp/workmanagement/master/TrackingActivity.java
+++ WorkManagement/app/src/main/java/ru/javaapp/workmanagement/master/TrackingActivity.java
@@ -12,7 +12,7 @@ public class TrackingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tracking);
+        setContentView(R.layout.activity_tracking);;
     }
 
     @Override
