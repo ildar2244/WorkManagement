4a03774b91bd6daa13afae1ddbaa2a666031c880
ildar2244 <ildar2244@gmail.com>
2015-10-13 15:43:51 +0300

    JSONParseWorker with URLConnection. Get data in TAB1.

--- WorkManagement/app/src/main/java/ru/javaapp/workmanagement/activities/TaskListActivity.java
+++ WorkManagement/app/src/main/java/ru/javaapp/workmanagement/activities/TaskListActivity.java
@@ -1,20 +1,33 @@
 package ru.javaapp.workmanagement.activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import ru.javaapp.workmanagement.MainActivity;
 import ru.javaapp.workmanagement.R;
+import ru.javaapp.workmanagement.worker.JSONParserWorker;
 
 public class TaskListActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private TabHost tabHost;
+    private String urlGetTasks = "http://autocomponent.motorcum.ru/get_tasks_by_worker.php";
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +35,7 @@ public class TaskListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_task_list);
         toolbarInitialize(); // init toolbar
         componentsInitialize(); //init components in activity
+        new JsonReadByWorker().execute();
     }
 
     /**
@@ -67,6 +81,8 @@ public class TaskListActivity extends AppCompatActivity {
         tapSpec.setContent(R.id.tab3);
         tapSpec.setIndicator("THREE");
         tabHost.addTab(tapSpec);
+
+        textView = (TextView) findViewById(R.id.tv_json);
     }
 
     /**
@@ -80,5 +96,74 @@ public class TaskListActivity extends AppCompatActivity {
             startActivity(new Intent(TaskListActivity.this, MainActivity.class));
             finish();
         } catch (Exception e) {}
+    }
+
+    /**
+     * We get the Tasks by Worker.
+     * Start AsyncTask to DB for getting JSON
+     */
+    public class JsonReadByWorker extends AsyncTask<String, String, JSONObject> {
+
+        JSONObject object;
+
+        @Override
+        protected JSONObject doInBackground(String... params) {
+
+            try {
+                JSONParserWorker parserWorker = new JSONParserWorker();
+                object = parserWorker.makeHttpRequest(urlGetTasks);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return object;
+        }
+
+        protected void onPostExecute(JSONObject json) {
+            try {
+                if(json != null) {
+                    ListDrawer(json);
+                }
+                else{
+                    AlertDialog.Builder builder = new AlertDialog.Builder(TaskListActivity.this,  R.style.AlertDialogStyle);
+                    builder.setCancelable(false);
+                    builder.setTitle("Ошибка");
+                    builder.setMessage("Нет соединения с интернетом.");
+                    builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() { // Кнопка ОК
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss(); // Отпускает диалоговое окно
+                        }
+                    });
+                    builder.show();
+                }
+            } catch (JSONException e) {
+                Toast.makeText(TaskListActivity.this, "Ошибка", Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void ListDrawer(JSONObject json) throws JSONException {
+        JSONArray jsonArray = null;
+        String resultText = "";
+
+        jsonArray = json.getJSONArray("allTasks_by_worker");
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            String nameMaster = jsonObject.getString("name");
+            String nameWorker = jsonObject.getString("nameWorker");
+            String nameWhat = jsonObject.getString("nameWhat");
+            String namePlace = jsonObject.getString("namePlace");
+            String countPlan = jsonObject.getString("count_plan");
+            String countCurrent = jsonObject.getString("count_current");
+            String timeStart = jsonObject.getString("time_start");
+            String timeFinish = jsonObject.getString("time_finish");
+            String dateStart = jsonObject.getString("date_start");
+            String dateFinish = jsonObject.getString("date_finish");
+            String comment = jsonObject.getString("comment");
+            resultText += nameMaster + " " + nameWorker + " " + nameWhat + " " + namePlace + " " + countPlan + " " + countCurrent + " " + timeStart + " " + timeFinish + " " + dateStart + " " + dateFinish + " " + comment;
+
+        }
+        textView.setText(resultText);
     }
 }
--- WorkManagement/app/src/main/java/ru/javaapp/workmanagement/worker/JSONParserWorker.java
+++ WorkManagement/app/src/main/java/ru/javaapp/workmanagement/worker/JSONParserWorker.java
@@ -1,75 +1,50 @@
 package ru.javaapp.workmanagement.worker;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by User on 13.10.2015.
  */
 public class JSONParserWorker {
 
-    private String urlGetTasks = "";
-    final int CONN_WAIT_TIME = 8000;
-    final int CONN_DATA_WAIT_TIME = 7000;
-    private String jsonResult;
+    HttpURLConnection urlConnection;
+    StringBuilder result = new StringBuilder();
+    URL urlGet;
     private JSONObject jsonObject;
 
     public JSONObject makeHttpRequest(String url) {
 
         try {
-            HttpParams httpParams = new BasicHttpParams();
-            HttpConnectionParams.setConnectionTimeout(httpParams, CONN_WAIT_TIME);
-            HttpConnectionParams.setSoTimeout(httpParams, CONN_DATA_WAIT_TIME);
+            urlGet = new URL(url);
+            urlConnection = (HttpURLConnection) urlGet.openConnection();
 
-            DefaultHttpClient postClient = new DefaultHttpClient(httpParams);
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 
-            HttpClient httpclient = postClient;
-            HttpPost httppost = new HttpPost(url);
-            HttpResponse response = httpclient.execute(httppost);
-            jsonResult = inputStreamToString(
-                    response.getEntity().getContent()).toString();
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+            }
+            jsonObject = new JSONObject(result.toString());
+
         } catch (IOException e) {
             e.printStackTrace();
-        }
-
-        try {
-            jsonObject = null;
-            jsonObject = new JSONObject(jsonResult);
         } catch (JSONException e) {
             e.printStackTrace();
         }
+        finally {
+            urlConnection.disconnect();
+        }
 
         return jsonObject;
-    }
-
-    private StringBuilder inputStreamToString(InputStream is) {
-        String rLine = "";
-        StringBuilder answer = new StringBuilder();
-        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-
-        try {
-            while ((rLine = rd.readLine()) != null) {
-                answer.append(rLine);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return answer;
     }
 }
