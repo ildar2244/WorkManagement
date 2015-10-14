af2048772de8d3359f9c92e600228ca1dcdbe0d6
ildar2244 <ildar2244@gmail.com>
2015-10-14 14:35:05 +0300

    in menu TLA add Sync-button

--- WorkManagement/app/src/main/java/ru/javaapp/workmanagement/activities/TaskListActivity.java
+++ WorkManagement/app/src/main/java/ru/javaapp/workmanagement/activities/TaskListActivity.java
@@ -10,6 +10,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TabHost;
 import android.widget.Toast;
@@ -217,5 +219,24 @@ public class TaskListActivity extends AppCompatActivity {
         rvTasksTwo.setAdapter(adaptersTasksThree);
         rvTasksTwo.setLayoutManager(llm2);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_task_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.sync) {
+            new JsonReadByWorker().execute(); // update RVs - get new data
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
